fdd391bbf31d95d41f8c8f00e1c6fe7818035b2b
qegabbe <qegabbe@outlook.de>
2016-04-29 11:43:51 +0200

    Update setup.bat

--- Tools/jenkins/setup.bat
+++ Tools/jenkins/setup.bat
@@ -1,11 +1,17 @@
+echo "Add Cygwin and Python to PATH"
+SET PATH=%PATH%;C:\Users\%USERNAME%\MyData\Installed\PortableApps\PortableApps\PortablePython2.7.6.1\App;C:\Users\%USERNAME%\MyData\Installed\cygwin\bin
+echo "Compile googletest and Link Library to PATH TODO"
 cd ..
 cd googletest-release-1.7.0/make
 make all
+echo "Compile Project"
 cd ..
 cd ..
 cd ..
 cd Debug
 make clean
 make all
+echo "Run Compiled Application"
 TestGTestCygwin.exe --gtest_output=xml:coverage.junit.xml
+echo "Generate Coverage Report"
 python gcovr -x -r .. -f".*src\\.*" > coverage.xml
