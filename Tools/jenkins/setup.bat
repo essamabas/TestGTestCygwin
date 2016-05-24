a662fe73272283f4ca18650f779607e4b47aaa9b
qegabbe <qegabbe@outlook.de>
2016-05-25 00:06:19 +0200

    prepare jenkins

--- Tools/jenkins/setup.bat
+++ Tools/jenkins/setup.bat
@@ -1,6 +1,7 @@
 echo "Add Cygwin and Python to PATH"
 echo %PATH%
 echo "Compile googletest and Link Library to PATH TODO"
+set OLDDIR=%CD%
 cd ..
 cd googletest-release-1.7.0/make
 make all
@@ -15,6 +16,8 @@ echo "Run Compiled Application"
 TestGTestCygwin.exe --gtest_output=xml:coverage.junit.xml
 echo "Generate Coverage Report"
 python gcovr -x -r .. -f".*src\\.*" > coverage.xml
+python gcovr -x -r .. -e".+\.src\\.*" > coverage2.xml
 echo "Move files to build-number Report"
 mkdir ..\..\builds\%BUILD_NUMBER%\workspace-files
 copy *.xml ..\..\builds\%BUILD_NUMBER%\workspace-files\*.xml
+chdir /d %OLDDIR% &rem restore current directory
--- setup.bat
+++
@@ -1,5 +0,0 @@
-cd Debug
-make clean
-make
-TestGTestCygwin.exe --gtest_output=xml:coverage.junit.xml
-python gcovr -x -r .. -e ".+\test\.cpp" > coverage.xml
